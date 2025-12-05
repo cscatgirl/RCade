79f8016dd49a93a8536f87352f0ecc4e924f7ed8
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-04 23:57:11 -0500

    RCADE_API_TOKEN -> CABINET_API_KEY

--- cabinet/scripts/install-service.sh
+++ cabinet/scripts/install-service.sh
@@ -49,7 +49,7 @@ Wants=graphical.target
 Type=simple
 User=$CURRENT_USER
 Environment=DISPLAY=:0
-Environment=RCADE_API_TOKEN=${RCADE_API_TOKEN:-}
+Environment=CABINET_API_KEY=${CABINET_API_KEY:-}
 ExecStart=$APP_PATH
 Restart=always
 RestartSec=5
